4df8991a7c81604c4f5fe0b716608bdb3701937b
Mehmood Khalil <38806418+iamwebsurgeon@users.noreply.github.com>
2023-03-23 02:54:50 +0500

    Create 01_hello_world.dart

---
+++ 01_hello_world.dart
@@ -0,0 +1,9 @@
+void main() {
+  print("Hello World");
+  print("This is our first dart programming application");
+  print('I am using single quote this time');
+  print(4);
+  print(10+5);
+  print(10-5);
+  print(true);
+}
